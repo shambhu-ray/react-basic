42d8dd614140a0a658a9b2169eabad88acc35c25
Shambhu <shambhuray.sam@gmail.com>
2019-09-11 09:05:41 +0530

    Updates

---
+++ precache-manifest.ea72c3f90d9c0732eeb2ccaa09cf6bf6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ecb8f75cb15dc13f3d89909dd115ac7",
+    "url": "/shambhu-ray/react-basic/index.html"
+  },
+  {
+    "revision": "9dffd5a8df1093699ae3",
+    "url": "/shambhu-ray/react-basic/static/css/main.071d3320.chunk.css"
+  },
+  {
+    "revision": "4b7594f9ba93f757104a",
+    "url": "/shambhu-ray/react-basic/static/js/2.765d0f25.chunk.js"
+  },
+  {
+    "revision": "9dffd5a8df1093699ae3",
+    "url": "/shambhu-ray/react-basic/static/js/main.79a81d20.chunk.js"
+  },
+  {
+    "revision": "ffbc84c0d6b7ad57745d",
+    "url": "/shambhu-ray/react-basic/static/js/runtime~main.384b8d52.js"
+  }
+]);
