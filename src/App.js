4c4a7f55d7ec7af58f79f8f7944ebe2a67b56816
Shambhu <shambhuray.sam@gmail.com>
2019-09-12 09:24:38 +0530

    extract version of setState

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, {Component} from 'react';
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
-import { CardList } from './components/card-list/card-list.component';
-import { SearcBox } from './components/search-box/search-box.component';
 
 
 class App extends Component {
@@ -10,35 +8,27 @@ class App extends Component {
     super();
     
     this.state = {
-      searchField: '',
-      monsters: []
+      meaningOfLife: 12
     };
   }
 
-  /**
-   * Life cucle hook
-   */
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({monsters: users}));
-  }
 
   handleChange = (event) => {
-    this.setState({searchField: event.target.value}, () => console.log(this.state));
+    this.setState((prevState, prevProps) => {
+      return {meaningOfLife: this.state.meaningOfLife + prevProps.increment}
+    },
+    () => console.log(this.state));
   }
 
   render() {
-    const {monsters, searchField} = this.state;
-    const filteredMonsters = monsters.filter(monster => 
-      monster.name.toLowerCase().includes(searchField.toLocaleLowerCase())
-    );
     return (
       <div className="App">
-        <h1>Monster Rolodex</h1>
-        <SearcBox placeholder='search monsters'
-        handleChange={this.handleChange} />
-        <CardList monsters={filteredMonsters}></CardList>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+
+          <h1>{this.state.meaningOfLife}</h1>
+          <button onClick={this.handleChange}>Update State</button>
+        </header>
       </div>
     );   
   }
